90e0bbe2efe9263f8beede4a69e44bd7fd7ebeb4
alshakh <ahmed.s.alshakh@gmail.com>
2016-03-19 18:23:27 -0600

    clean aliases

--- aliases.sh
+++ aliases.sh
@@ -2,40 +2,11 @@
 
 alias _=sudo
 
-alias ls='ls -l --color=auto'
-alias lsa="ls -a"
+alias ls='ls --color=auto'
+alias lls='ls -l --color=auto'
+alias lsa="ls -a --color=auto"
 
 alias ..="cd .."
 alias ...="cd ../.."
 
 alias put="xsel" # Paste from clipboard to terminal
-
-# Git
-alias g=git
-alias gcl="git clone"
-alias ga="git add"
-alias gall="git add -A"
-alias gc="git commit -v"
-alias gcm="git commit -v -m"
-alias gm="git merge"
-alias grb="git rebase -i"
-alias gst="git status"
-alias gs="git status -s"
-alias grmt="git remote"
-alias gpl="git pull"
-alias gft="git fetch --all --prune --tags --verbose"
-alias gp="git push"
-alias gpo="git push origin"
-alias gpom="git push origin master"
-alias gb="git branch"
-alias gco="git checkout"
-alias glg="git log"
-alias gl="git log --oneline"
-alias gd="git diff"
-alias gta="git tag -a"
-alias gr="git reset"
-alias grh="git reset HEAD"
-alias grhh="git reset HEAD --hard"
-
-
-
